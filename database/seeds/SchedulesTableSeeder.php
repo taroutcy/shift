9f4150fdb7d3749c880e9ad650ae50fe66ecdef8
taroutcy <taroutcy@gmail.com>
2022-10-28 19:52:14 +0900

    [clean] seeder

--- database/seeds/SchedulesTableSeeder.php
+++ database/seeds/SchedulesTableSeeder.php
@@ -11,48 +11,48 @@ class SchedulesTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('schedules') -> insert([
-            [
-                'id' => '1', 
-                'user_id' => '1', 
-                'shift_id' => '1', 
-                'schedule_status_id' => '1', 
-                'work_status_id' => '1', 
-                'date' => '2022-10-02', 
-                'created_at' => null, 
-                'updated_at' => null
-            ], 
-            [
-                'id' => '2', 
-                'user_id' => '1', 
-                'shift_id' => '2', 
-                'schedule_status_id' => '1', 
-                'work_status_id' => '1', 
-                'date' => '2022-10-03', 
-                'created_at' => null, 
-                'updated_at' => null
-            ], 
-            [
-                'id' => '3', 
-                'user_id' => '1', 
-                'shift_id' => null, 
-                'schedule_status_id' => '1', 
-                'work_status_id' => '2', 
-                'date' => '2022-10-04', 
-                'created_at' => null, 
-                'updated_at' => null
-            ], 
-            [
-                'id' => '4', 
-                'user_id' => '1', 
-                'shift_id' => null, 
-                'schedule_status_id' => '1', 
-                'work_status_id' => '3', 
-                'date' => '2022-10-05', 
-                'created_at' => null, 
-                'updated_at' => null
-            ], 
+        // DB::table('schedules') -> insert([
+        //     [
+        //         'id' => '1', 
+        //         'user_id' => '1', 
+        //         'shift_id' => '1', 
+        //         'schedule_status_id' => '1', 
+        //         'work_status_id' => '1', 
+        //         'date' => '2022-10-02', 
+        //         'created_at' => null, 
+        //         'updated_at' => null
+        //     ], 
+        //     [
+        //         'id' => '2', 
+        //         'user_id' => '1', 
+        //         'shift_id' => '2', 
+        //         'schedule_status_id' => '1', 
+        //         'work_status_id' => '1', 
+        //         'date' => '2022-10-03', 
+        //         'created_at' => null, 
+        //         'updated_at' => null
+        //     ], 
+        //     [
+        //         'id' => '3', 
+        //         'user_id' => '1', 
+        //         'shift_id' => null, 
+        //         'schedule_status_id' => '1', 
+        //         'work_status_id' => '2', 
+        //         'date' => '2022-10-04', 
+        //         'created_at' => null, 
+        //         'updated_at' => null
+        //     ], 
+        //     [
+        //         'id' => '4', 
+        //         'user_id' => '1', 
+        //         'shift_id' => null, 
+        //         'schedule_status_id' => '1', 
+        //         'work_status_id' => '3', 
+        //         'date' => '2022-10-05', 
+        //         'created_at' => null, 
+        //         'updated_at' => null
+        //     ], 
             
-        ]);
+        // ]);
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -24,17 +24,6 @@ class UsersTableSeeder extends Seeder
                 'active' => '1', 
                 'editor' => '1'
             ],
-            [
-                'id' => '2', 
-                'password' => Hash::make('testtest'), 
-                'role_id' => '3', 
-                'department_id' => '2', 
-                'last_name' => '坂本', 
-                'first_name' => '花子', 
-                'number' => '0061630090',
-                'active' => '1', 
-                'editor' => '0'
-            ],
         ]);
     }
 }
