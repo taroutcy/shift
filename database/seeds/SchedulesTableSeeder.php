31162e6cc2e65f6bb0d22b558b6fc8c28639ebad
taroutcy <taroutcy@gmail.com>
2023-01-25 22:41:39 +0900

    [set] shift seeder

--- database/seeds/SchedulesTableSeeder.php
+++ database/seeds/SchedulesTableSeeder.php
@@ -11,48 +11,52 @@ class SchedulesTableSeeder extends Seeder
      */
     public function run()
     {
-        // DB::table('schedules') -> insert([
-        //     [
-        //         'id' => '1', 
-        //         'user_id' => '1', 
-        //         'shift_id' => '1', 
-        //         'schedule_status_id' => '1', 
-        //         'work_status_id' => '1', 
-        //         'date' => '2022-10-02', 
-        //         'created_at' => null, 
-        //         'updated_at' => null
-        //     ], 
-        //     [
-        //         'id' => '2', 
-        //         'user_id' => '1', 
-        //         'shift_id' => '2', 
-        //         'schedule_status_id' => '1', 
-        //         'work_status_id' => '1', 
-        //         'date' => '2022-10-03', 
-        //         'created_at' => null, 
-        //         'updated_at' => null
-        //     ], 
-        //     [
-        //         'id' => '3', 
-        //         'user_id' => '1', 
-        //         'shift_id' => null, 
-        //         'schedule_status_id' => '1', 
-        //         'work_status_id' => '2', 
-        //         'date' => '2022-10-04', 
-        //         'created_at' => null, 
-        //         'updated_at' => null
-        //     ], 
-        //     [
-        //         'id' => '4', 
-        //         'user_id' => '1', 
-        //         'shift_id' => null, 
-        //         'schedule_status_id' => '1', 
-        //         'work_status_id' => '3', 
-        //         'date' => '2022-10-05', 
-        //         'created_at' => null, 
-        //         'updated_at' => null
-        //     ], 
-            
-        // ]);
+        $months = range(1,12);
+        $days = range(1,30);
+        
+        // admin seeder 
+        $count = 1;
+        foreach($months as $month) {
+            foreach($days as $day) {
+                if($month == 2 && $day > 28) {
+                    break;
+                } 
+                
+                DB::table('schedules') -> insert([
+                    'id' => $count,
+                    'user_id' => '1', 
+                    'shift_id' => rand(1,2), 
+                    'schedule_status_id' => '1', 
+                    'work_status_id' => rand(1,3), 
+                    'date' => '2023-'.str_pad($month, 2, 0, STR_PAD_LEFT).'-'.str_pad($day, 2, 0, STR_PAD_LEFT), 
+                    'created_at' => null,
+                    'updated_at' => null
+                ]);
+
+                $count++;
+            }
+        }
+        
+        // staff1 seeder
+        foreach($months as $month) {
+            foreach($days as $day) {
+                if($month == 2 && $day > 28) {
+                    break;
+                } 
+                
+                DB::table('schedules') -> insert([
+                    'id' => $count,
+                    'user_id' => '2', 
+                    'shift_id' => rand(1,9), 
+                    'schedule_status_id' => '1', 
+                    'work_status_id' => '1', 
+                    'date' => '2023-'.str_pad($month, 2, 0, STR_PAD_LEFT).'-'.str_pad($day, 2, 0, STR_PAD_LEFT), 
+                    'created_at' => null,
+                    'updated_at' => null
+                ]);
+
+                $count++;
+            }
+        }
     }
 }
