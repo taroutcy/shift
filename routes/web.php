d62e19ecb6f20436585d59c8fd9efae038f06523
taro0418 <taroutcy@gmail.com>
2024-01-31 12:18:44 +0900

    [success] moving to local

--- routes/web.php
+++ routes/web.php
@@ -21,27 +21,27 @@ Auth::routes([
 ]);
 
 
-Route::group(['middleware' => 'auth'], function() {
-     // ここにログイン後のみのルーティングを記述
+Route::group(['middleware' => 'auth'], function () {
+    // ここにログイン後のみのルーティングを記述
     Route::get('/home', 'HomeController@index')->name('home');
     Route::get('/', 'HomeController@index');
-    
-    Route::group(['prefix' => 'user'], function() {
+
+    Route::group(['prefix' => 'user'], function () {
         Route::get('register', 'UserController@getRegister')->name('user.register.get');
         Route::get('edit/{id}', 'UserController@getEdit')->name('user.edit.get');
         Route::post('edit/{id}', 'UserController@postEdit')->name('user.edit.post');
         Route::get('home', 'UserController@home')->name('user.home');
         Route::post('register', 'UserController@postRegister')->name('user.register.post');
     });
-    
-    Route::group(['prefix' => 'shift'], function() {
+
+    Route::group(['prefix' => 'shift'], function () {
         Route::get('calendar/{year?}/{month?}', 'ScheduleController@getShift')->name('shift.calendar.edit');
         Route::post('calendar/{date?}', 'ScheduleController@postShift')->name('shift.calendar.post');
-        Route::get('check/{year?}/{month?}','ScheduleController@checkShift')->name('shift.check');
-        Route::get('confirm/{year?}/{month?}','ScheduleController@getConfirmShift')->name('shift.confirm.get');
-        Route::post('confirm/{year?}/{month?}','ScheduleController@allConfirmShift')->name('shift.confirm.all');
-        Route::post('confirm/{id?}/{year?}/{month?}/{date?}','ScheduleController@changeConfirmShift')->name('shift.confirm.change');
+        Route::get('check/{year?}/{month?}', 'ScheduleController@checkShift')->name('shift.check');
+        Route::get('confirm/{year?}/{month?}', 'ScheduleController@getConfirmShift')->name('shift.confirm.get');
+        Route::post('confirm/{year?}/{month?}', 'ScheduleController@allConfirmShift')->name('shift.confirm.all');
+        Route::post('confirm/{id?}/{year?}/{month?}/{date?}', 'ScheduleController@changeConfirmShift')->name('shift.confirm.change');
     });
- });
- 
- URL::forceScheme('https');
+});
+
+URL::forceScheme('http');
